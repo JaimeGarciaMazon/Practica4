58bac9f28e2dae628297f07bb66e4f9210e64938
jaime <jaimegarcia_24@hotmail.com>
2019-01-16 18:27:57 +0100

    Actualizacion

---
+++ app/src/main/java/com/example/jaime/pratica4/DesplazandoImagen.java
@@ -0,0 +1,27 @@
+package com.example.jaime.pratica4;
+
+
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class DesplazandoImagen extends AppCompatActivity implements FragmentImagen1.OnFragmentInteractionListener,FragmentImagen2.OnFragmentInteractionListener{
+
+    Fragment FragmentImagen1;
+    Fragment FragmentImagen2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desplazando_imagen);
+    }
+
+    @Override
+    public void onFragmentInteraction (Uri Uri){
+
+
+    }
+}
